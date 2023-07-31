c497e25fb32413cd777e89553965c8224a269384
Harish Verma <87308425+Harishv2002@users.noreply.github.com>
2023-07-31 10:25:36 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected ${conn.connection.host}`);
+    console.log(`MongoDB Connected`);
+    // console.log(`MongoDB Connected ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error : ${error.message}`);
     process.exit(1);
